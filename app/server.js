26667837bc71274e8188dfa81e66902c077b686c
Yi Fei Cheng <ycheng2@swarthmore.edu>
2018-02-17 14:37:16 +0800

    Prepared file for temba.

--- app/server.js
+++ app/server.js
@@ -4,15 +4,17 @@ var express = require("express");
 // Import firebase
 var admin = require('firebase-admin');
 // Import service account
-var serviceAccount = require('./serviceAccountKey.json');
+//var serviceAccount = require('./serviceAccountKey.json');
 
 // Initialize express.js app
 var app = express();
 // Initialize firebase app
+/*
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://nrafunded-55558.firebaseio.com"
 });
+*/
 
 // Define the view engine: EJS
 // EJS: Effective JavaScript (Templating language)
@@ -31,7 +33,7 @@ app.use(require('./routes/index'));
 app.set('port', process.env.PORT || 3000 );
 
 // Initialize database
-var db = admin.database();
+//var db = admin.database();
 
 //Serve the application
 var server = app.listen(app.get('port'), function() {
