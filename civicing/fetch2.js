7cf7f2103cd69c7c2bb748f2e46f694ba766da4d
kevzheng1 <35941399+kevzheng1@users.noreply.github.com>
2018-02-17 09:56:13 -1000

    some minor changes

--- civicing/fetch2.js
+++ civicing/fetch2.js
@@ -2,38 +2,34 @@
 // this one requests local representatives based on an address, and stores it in the "data" field
 // apikey not working right now cuz I can't use require
 function saveReps(address){
-	var config = require('C:\\Users\\Kevin Zheng\\Documents\\Grade 0\\HopHack\\apee.json');
+	var config = "AIzaSyDL4fVkLF6vdgzjt4C0XApfOmaH2Fciyok";
 	var newaddress = changeaddress(address);
 	console.log(newaddress);
-	$.get("https://www.googleapis.com/civicinfo/v2/representatives?key=" + config.apikey + "&address=" + newaddress, function(data, status){
+	$.get("https://www.googleapis.com/civicinfo/v2/representatives?key=" + config + "&address=" + newaddress, function(data, status){
 		// the data variable holds the information you seek
 		console.log(status);
 		console.log(data);
 	});
-	console.log(reps);
-	return reps;
 }
 
 // this one requests all elections that can be queried at the time.
 // each election has a unique ID number that can be used in the next function to find more details
 function saveElections(){
-	var config = require('C:\\Users\\Kevin Zheng\\Documents\\Grade 0\\HopHack\\apee.json');
-	$.get("https://www.googleapis.com/civicinfo/v2/elections?key=" + config.apikey, function(data, status){
+	var config = "AIzaSyDL4fVkLF6vdgzjt4C0XApfOmaH2Fciyok";
+	$.get("https://www.googleapis.com/civicinfo/v2/elections?key=" + config, function(data, status){
 		console.log(status);
 		console.log(data);
 	});
-	return elections;
 }
 
 // takes an address and election ID number to find more detailed information about a specific election
 function saveOneElection(address, electionId){
-	var config = require('C:\\Users\\Kevin Zheng\\Documents\\Grade 0\\HopHack\\apee.json');/
+	var config = "AIzaSyDL4fVkLF6vdgzjt4C0XApfOmaH2Fciyok";
 	var newaddress = changeaddress(address);
-	$.get("https://www.googleapis.com/civicinfo/v2/voterinfo?key=" + config.apikey +"&address=" + newaddress + "&electionId=" + electionId , function(data, status){
+	$.get("https://www.googleapis.com/civicinfo/v2/voterinfo?key=" + config +"&address=" + newaddress + "&electionId=" + electionId , function(data, status){
 		console.log(status);
 		console.log(data);
 	});
-	return oneElection;
 }
 
 // basic parsing
@@ -46,8 +42,7 @@ function changeaddress(address){
 // lots of testing happening down there
 
 // still have to work on successfully returning the data from the functions
-//var elections = saveElections();
-//var kansasReps = saveReps('1263 Pacific Ave., Kansas City, KS');
-//var swatReps = saveReps('500 College Ave, Swarthmore, PA');
-//var oneElection = saveOneElection('500 College Ave, Swarthmore, PA', 2000);
-var latlong = saveReps('39.906841, -75.355648');
+saveElections();
+saveReps('1263 Pacific Ave., Kansas City, KS');
+saveOneElection('500 College Ave, Swarthmore, PA', 2000);
+saveReps('37.968056, -77.746667');
