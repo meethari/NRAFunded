b944205e4d5c8cdf5e99055100d78375802697b8
meethari <srinivasulu.hari@gmail.com>
2018-02-17 20:38:16 -0500

    Added comparison code

---
+++ comparison_code/crap.js
@@ -0,0 +1,12 @@
+/*
+if (lastName === "Jr" || lastName === ".Jr")
+{
+  for (i = 0; i < name.length && name.charAt(i) !== " "; i++){
+  }
+  var start = i;
+  for (i = name.length - 1; i >= 0 && name.charAt(i) !== " "; i--){
+  }
+  var end = i;
+  lastName = name.substring(start + 1, end);
+}
+*/
